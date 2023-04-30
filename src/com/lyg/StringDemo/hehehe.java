4346e7804f369e768f4b25a627888173c35bd6b1
eminent123 <3370084577@qq.com>
2023-04-30 23:43:16 +0800

    hehehe

---
+++ src/com/lyg/StringDemo/hehehe.java
@@ -0,0 +1,11 @@
+package com.lyg.StringDemo;
+
+/**
+ * @author almost
+ * @date 2023/4/30 23:41
+ */
+public class hehehe {
+    public static void main(String[] args) {
+        System.out.println("hehehe");
+    }
+}
