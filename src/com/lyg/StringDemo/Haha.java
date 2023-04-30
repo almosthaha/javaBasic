22c3a6f6c84e92d2f84cf26434a2d14c444b0ffb
eminent123 <3370084577@qq.com>
2023-04-30 20:04:42 +0800

    这是测试初始化本地库的作用

---
+++ src/com/lyg/StringDemo/Haha.java
@@ -0,0 +1,11 @@
+package com.lyg.StringDemo;
+
+/**
+ * @author almost
+ * @date 2023/4/30 20:02
+ */
+public class Haha {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
